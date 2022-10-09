c8c64225d72111669d14940e21bfcc080a6636d8
AnanyaDas162 <myselfadas00@gmail.com>
2022-10-09 18:36:19 +0530

    Making Compact

---
+++ LeetCode/C++/count_subtrees.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+int countSubtreesWithSumX(Node* root, int x);
+
+int main()
+{
+  int t;
+  cin>>t;
+  getchar();
+  while (t--)
+  {
+     string s;
+     getline(cin,s);
+     Node* root = buildTree(s);
+     
+     int x;
+     cin>>x;
+     getchar();
+     cout << countSubtreesWithSumX(root, x)<<endl;
+  }
+  return 0;
+}
+
+/*
+Structure of the node of the binary tree is as
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+*/
+
+int countSubtreesWithSumX(Node* root, int X)
+{
+
+	int ans=0;
+    if(root->left){
+        ans+=countSubtreesWithSumX(root->left,X);
+        root->data+=root->left->data;
+    }
+    if(root->right){
+        ans+=countSubtreesWithSumX(root->right,X);
+        root->data+=root->right->data;
+    }
+    if(root->data==X)ans++;
+    return ans;
+}
+
--- Sorting Algorithm/C ++/quickSort.cpp
+++ Sorting Algorithm/C ++/quickSort.cpp
@@ -1,65 +1,73 @@
-/*Program for quick sort implementation in C++. */
+/* C++ implementation of QuickSort */
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <iostream>
-#include <vector>
-
-int partition(std::vector<int> &A, int start, int end)
+// A utility function to swap two elements
+void swap(int* a, int* b)
 {
-	int temp;
-	int pivot = A[end];
-	int partitionIndex = start; // set partition index as start initially
-	for(int i=start; i<end; i++)
-	{
-		if(A[i] <= pivot)
-		{
-			std::swap(A[i], A[partitionIndex]);
-			partitionIndex++;
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high]; // pivot
+	int i
+		= (low
+		- 1); // Index of smaller element and indicates
+				// the right position of pivot found so far
+
+	for (int j = low; j <= high - 1; j++) {
+		// If current element is smaller than the pivot
+		if (arr[j] < pivot) {
+			i++; // increment index of smaller element
+			swap(&arr[i], &arr[j]);
 		}
 	}
-	std::swap(A[partitionIndex], A[end]);		//	partition index
-
-	return partitionIndex;
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
 }
 
-void swapElements(int* elementOne, int* elementTwo)
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+void quickSort(int arr[], int low, int high)
 {
-	int temp = *elementOne;
-	*elementOne = *elementTwo;
-	*elementTwo = temp;
-}
-std::vector<int> quick_sort(std::vector<int> &A, int start, int end)
-{
-/*        std::cout<<"Current list : \n";
-        for(int i=0;i<A.size();i++)
-        {
-                std::cout<<A[i]<<" ";
-        }
-        std::cout<<"\nSorted list : \n";
-*/
+	if (low < high) {
+		/* pi is partitioning index, arr[p] is now
+		at right place */
+		int pi = partition(arr, low, high);
 
-	if(start<end)
-	{
-		int partitionIndex = partition(A,start,end);
-		quick_sort(A,start,partitionIndex-1);
-		quick_sort(A,partitionIndex+1, end);
+		// Separately sort elements before
+		// partition and after partition
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
 	}
-
-/*        for(int i=0;i<A.size();i++)
-        {
-                std::cout<<A[i]<<" ";
-        }
-*/        return A;
 }
 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+// Driver Code
 int main()
 {
-        std::cout<<"\nTest\n";
-        std::vector<int> arr={10,2,23,-4,235,56,2,6,5,5,5,23,-4,346,-56,81,43,654,435,-54};
-        quick_sort(arr, 0, arr.size()-1);
-        std::cout<<"\n\n\n";
-        for(int i=0;i<arr.size();i++)
-        {
-                std::cout<<arr[i]<<" ";
-        }
-        return 0;
+	int arr[] = { 10, 7, 8, 9, 1, 5 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	quickSort(arr, 0, n - 1);
+	cout << "Sorted array: \n";
+	printArray(arr, n);
+	return 0;
 }
