35de0207e749ed15723ef1ddef2ffbe98961772a
SoumikMondal01 <soumikmondal663@gmail.com>
2022-10-13 16:09:33 +0530

    added leetcode 78 subsets in JAVA

---
+++ LeetCode/Java/78.subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+       List<List<Integer>> ll=new ArrayList();
+       generate(nums,0,new ArrayList(),ll);
+       return ll;
+   }
+   public void generate(int[] nums,int i,List<Integer> ls,List<List<Integer>> ll){
+       if(i==nums.length){
+           ll.add(new ArrayList(ls));
+           return;
+       }
+       generate(nums,i+1,ls,ll);
+       ls.add(nums[i]);
+       generate(nums,i+1,ls,ll);
+       ls.remove(ls.size()-1);
+    }
+       
+}
