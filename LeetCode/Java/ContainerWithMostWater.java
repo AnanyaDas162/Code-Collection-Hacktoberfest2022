6d3cdc90ee51b53e1ccc341b88af5230e28617b0
sayanGIT-07 <106771478+sayanGIT-07@users.noreply.github.com>
2022-10-15 21:44:35 +0530

    Created ContainerWithMostWater.java

--- LeetCode/Java/ContainerWithMostWater.java
+++ LeetCode/Java/ContainerWithMostWater.java
@@ -1,24 +1,22 @@
-
-
-class ContainerWithMostWater {
-	 public int maxArea(int[] height) {
-	        int l=0;
-	        int h=height.length-1;
-	        
-	        int max=0;
-	        
-	        while(l<h){
-	            int lh=height[l];
-	            int rh=height[h];
-	            int min_h=Math.min(lh,rh);
-	            max=Math.max(max,min_h*(h-l));
-	        
-	            if(min_h==lh)
-	                l++;
-	            else
-	                h--;
-	        }
-	        return max;
-	        
-	    }
+class Solution {
+    public int maxArea(int[] height) {
+        int l=0;
+        int h=height.length-1;
+        
+        int max=0;
+        
+        while(l<h){
+            int lh=height[l];
+            int rh=height[h];
+            int min_h=Math.min(lh,rh);
+            max=Math.max(max,min_h*(h-l));
+        
+            if(min_h==lh)
+                l++;
+            else
+                h--;
+        }
+        return max;
+        
+    }
 }
