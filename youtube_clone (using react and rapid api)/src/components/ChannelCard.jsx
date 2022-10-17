bbfcf52f19784ff03750937ceb2dd8cde00dbd4c
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-10-17 16:13:55 +0530

    Added youtube_clone project
    Made using react and rapid api use.

---
+++ youtube_clone (using react and rapid api)/src/components/ChannelCard.jsx
@@ -0,0 +1,53 @@
+import { CheckCircle } from "@mui/icons-material";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
+
+const ChannelCard = ({ channelDetail, marginTop }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "356px", md: "320px" },
+      height: "326px",
+      margin: "auto",
+      marginTop,
+    }}>
+    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "#fff",
+        }}>
+        <CardMedia
+          image={
+            channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
+          }
+          alt={channelDetail?.snippet?.title}
+          sx={{
+            borderRadius: "50%",
+            height: "180px",
+            width: "180px",
+            mb: 2,
+            border: "1px solid #e3e3e3",
+          }}
+        />
+        <Typography variant='h6'>
+          {channelDetail?.snippet?.title}
+          <CheckCircle sx={{ fontSize: 15, color: "grey", ml: "5px" }} />
+        </Typography>
+        <Typography variant='h7'>
+          {channelDetail?.statistics?.subscriberCount}
+        </Typography>
+      </CardContent>
+    </Link>
+  </Box>
+);
+
+export default ChannelCard;
---
+++ youtube_clone (using react and rapid api)/src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Navbar } from "./Navbar";
+export { default as Feed } from "./Feed";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as SearchBar } from "./SearchBar";
+export { default as Sidebar } from "./Sidebar";
+export { default as Videos } from "./Videos";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
+export { default as Loader } from "./Loader";
