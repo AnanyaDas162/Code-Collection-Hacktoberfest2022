b5649a8b4893c6a1781553a0772b5f1afab1405d
Arkajyoti Mukhopadhyay <91543992+arkajyotim@users.noreply.github.com>
2022-10-17 20:48:49 +0530

    Circular Array Loop

---
+++ LeetCode/C++/Circular Array Loop.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    int next(vector<int>& nums, int i){
+        int n = nums.size();
+        return (n+nums[i]+i)%n;
+    }
+    
+    bool circularArrayLoop(vector<int>& nums) {
+        int n = nums.size();
+        // we can use slow and fast pointer to check whether there is loop or not
+        for(int &num: nums)
+            num %= n;
+        for(int i=0;i<n;i++){
+            int slow = i,
+                fast = i;
+            while(nums[slow]*nums[next(nums,fast)]>0 && nums[slow]*nums[next(nums,next(nums,fast))]>0){
+                slow = next(nums,slow);
+                fast = next(nums,next(nums,fast));
+                if(slow==fast){
+                    if(slow==next(nums,slow)) // single length
+                        return false;
+                    return true;
+                }
+            }
+			/// DONOT TRAVERSE WHERE THERE IS NO PATH TO GET LOOP.
+            int j = i;
+            int val = nums[i];
+            while (nums[j] * val > 0) {
+                int nexx = next(nums,j);
+                nums[j] = 0;
+                j = nexx;
+            }
+        }
+        
+        return false;
+    }
+};
